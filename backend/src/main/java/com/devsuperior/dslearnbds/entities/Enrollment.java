3b266ecd4353ffa9b43a060a00a49ab07c98a74b
Alan Pacheco <alan.estudo@gmail.com>
2022-02-12 14:36:08 -0300

    Add Deliver, Topic and Reply entities

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -2,12 +2,14 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
@@ -29,6 +31,9 @@ public class Enrollment {
 	
 	@ManyToMany(mappedBy = "enrollmentsDone")
 	private Set<Lesson> lessonsDone = new HashSet<>();
+	
+	@OneToMany(mappedBy = "enrollment")
+	private List<Deliver> deliveries;
 	
 	public Enrollment() {
 	}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -26,23 +26,42 @@ INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prere
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d', 1, 1);
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d', 1, 2);
 
-INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
-INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
 
-INSERT INTO tb_lesson(title, position,section_id) VALUES ('Aula 1 do Capítulo 1', 1,1);
-INSERT INTO tb_lesson(title, position,section_id) VALUES ('Aula 2 do Capítulo 1', 2,1);
-INSERT INTO tb_lesson(title, position,section_id) VALUES ('Aula 3 do Capítulo 1', 3,1);
-INSERT INTO tb_lesson(title, position,section_id) VALUES ('Tarefa do Capítulo 1', 4,1);
+INSERT INTO tb_lesson (title, position,section_id) VALUES ('Aula 1 do Capítulo 1', 1,1);
+INSERT INTO tb_lesson (title, position,section_id) VALUES ('Aula 2 do Capítulo 1', 2,1);
+INSERT INTO tb_lesson (title, position,section_id) VALUES ('Aula 3 do Capítulo 1', 3,1);
+INSERT INTO tb_lesson (title, position,section_id) VALUES ('Tarefa do Capítulo 1', 4,1);
 
-INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (1, 'Material de apoio abc','https://www.youtube.com/watch?v=WR4_chcwvoM&list=RDMM&start_radio=1&rv=HrMb2oZgujE');
-INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (2, 'Material de apoio abc','https://www.youtube.com/watch?v=WR4_chcwvoM&list=RDMM&start_radio=1&rv=HrMb2oZgujE');
-INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (3, 'Material de apoio abc','https://www.youtube.com/watch?v=WR4_chcwvoM&list=RDMM&start_radio=1&rv=HrMb2oZgujE');
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'Material de apoio abc','https://www.youtube.com/watch?v=WR4_chcwvoM&list=RDMM&start_radio=1&rv=HrMb2oZgujE');
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, 'Material de apoio abc','https://www.youtube.com/watch?v=WR4_chcwvoM&list=RDMM&start_radio=1&rv=HrMb2oZgujE');
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, 'Material de apoio abc','https://www.youtube.com/watch?v=WR4_chcwvoM&list=RDMM&start_radio=1&rv=HrMb2oZgujE');
 
-INSERT INTO tb_task(id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2021-11-25T13:00:00Z');
+INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2021-11-25T13:00:00Z');
 
-INSERT INTO tb_lessons_done(lesson_id,user_id, offer_id) VALUES (1,1,1);
-INSERT INTO tb_lessons_done(lesson_id,user_id, offer_id) VALUES (2,1,1);
+INSERT INTO tb_lessons_done (lesson_id,user_id, offer_id) VALUES (1,1,1);
+INSERT INTO tb_lessons_done (lesson_id,user_id, offer_id) VALUES (2,1,1);
 
-INSERT INTO tb_notification(text, moment, read, route, user_id) VALUES ('Primeiro feedback da tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', false, 'offers/1/resource/1/sections/1', 1);
-INSERT INTO tb_notification(text, moment, read, route, user_id) VALUES ('Segundo feedback da tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', false, 'offers/1/resource/1/sections/1', 1);
-INSERT INTO tb_notification(text, moment, read, route, user_id) VALUES ('Terceiro feedback da tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', false, 'offers/1/resource/1/sections/1', 1);
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Primeiro feedback da tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', false, 'offers/1/resource/1/sections/1', 1);
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Segundo feedback da tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', false, 'offers/1/resource/1/sections/1', 1);
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Terceiro feedback da tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', false, 'offers/1/resource/1/sections/1', 1);
+
+INSERT INTO tb_deliver (uri, moment, status, feedback, correct_Count, lesson_id, user_id, offer_id) VALUES ('https://www.youtube.com/watch?v=WR4_chcwvoM&list=RDMM&start_radio=1&rv=HrMb2oZgujE', TIMESTAMP WITH TIME ZONE '2021-11-30T13:00:00Z', 0, null, null, 4, 1, 1);
+INSERT INTO tb_deliver (uri, moment, status, feedback, correct_Count, lesson_id, user_id, offer_id) VALUES ('https://www.youtube.com/watch?v=WR4_chcwvoM&list=RDMM&start_radio=1&rv=HrMb2oZgujE', TIMESTAMP WITH TIME ZONE '2021-11-30T13:00:00Z', 0, null, null, 4, 2, 1);
+INSERT INTO tb_deliver (uri, moment, status, feedback, correct_Count, lesson_id, user_id, offer_id) VALUES ('https://www.youtube.com/watch?v=WR4_chcwvoM&list=RDMM&start_radio=1&rv=HrMb2oZgujE', TIMESTAMP WITH TIME ZONE '2021-11-30T13:00:00Z', 0, null, null, 3, 2, 1);
+
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES('Título do tópico 1', 'Corpo do tópico 1', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', 1, 1, 1);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES('Título do tópico 2', 'Corpo do tópico 2', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z',2, 1, 1);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES('Título do tópico 3', 'Corpo do tópico 3', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z',2, 1, 1);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES('Título do tópico 4', 'Corpo do tópico 4', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z',1, 1, 2);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES('Título do tópico 5', 'Corpo do tópico 5', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z',1, 1, 2);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES('Título do tópico 6', 'Corpo do tópico 6', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z',2, 1, 3);
+
+INSERT INTO tb_topic_likes (topic_id, user_id) VALUES (1, 2);
+INSERT INTO tb_topic_likes (topic_id, user_id) VALUES (2, 1);
+
+INSERT INTO tb_reply (body, moment, topic_id, author_id) VALUES ('Tente reiniciar o computador', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', 1, 2);
+INSERT INTO tb_reply (body, moment, topic_id, author_id) VALUES ('Deu certo valeu!', TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', 1, 1);
+
+INSERT INTO tb_reply_likes (reply_id, user_id) VALUES (1,1);
